f0e2fafc83d19ba1a56214f5a56597c6f003166e
Vinicius Kalach <viniciuscato@hotmail.com>
2023-10-07 18:49:12 -0300

    Documentar o código. Etapa 3

--- src/login/User.java
+++ src/login/User.java
@@ -5,17 +5,40 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/**
+ * 
+ * @author Daniel Ohata
+ *
+ * A classe lida com a autenticação de usuários em um banco de dados MySQL.
+ * Ela possui métodos para conectar ao banco, verificar os dados do usuário e armazenar as suas informações.
+ */
 public class User {
+	/**
+	 * 
+	 * Método utilizado para conectar com o banco de dados MySQL.
+	 */
 	public Connection conectarDB() {
 		Connection conn = null;
 		try {
+			// Carrega a classe do driver JDBC MySQL.
 			Class.forName("com.mysql.Driver.Manager").newInstance();
+			 // Conexão do banco de dados MySQL, usuário e senha.
 			String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
 			conn = DriverManager.getConnection(url);
-		} catch (Exception e) { }
+		} catch (Exception e) { 
+			// Espaço para a manipulação de exceções.
+		}
 		return conn;}
+// Variavel que carrega o nome do usuário.
 public String nome="";
+// Variável que representa o resultado da verificação do usuário com o banco.
 public boolean result = false;
+/**
+ * Método que verifica as credenciais do usuário.
+ * @param login O nome do usuário.
+ * @param senha A senha do usuário.
+ * @return Se a verificação der válido, retorna 'true', senão retornará 'false'.
+ */
 public boolean verificarUsuario(String login, String senha) {
 	String sql = "";
 	Connection conn = conectarDB();
@@ -27,8 +50,11 @@ public boolean verificarUsuario(String login, String senha) {
 		Statement st = conn.createStatement();
 		ResultSet rs = st.executeQuery(sql);
 		if(rs.next()) {
+			// Se retornar uma linha, as credenciais são válidas.
 			result = true;
 			nome = rs.getString("nome");}
-		} catch (Exception e) {	}
+		} catch (Exception e) {	
+			// Espaço para a manipulação de exceções.
+		}
 	return result;}
 	}//fim da class
